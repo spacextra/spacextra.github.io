ee01205a8f043ef4f8028a3b53d1f53560243504
Charlie Kingsland <18499854+ChopsKingsland@users.noreply.github.com>
2022-07-16 22:08:46 +0100

    launch info for sharing

--- launch/index.js
+++ launch/index.js
@@ -3,4 +3,16 @@ const urlParams = new URLSearchParams(queryString);
 
 if (urlParams.has('id')) {
     const launchID = urlParams.get('id');
+    // document.getElementById('test').innerHTML = launchID;
+
+    $.getJSON(`https://api.spacexdata.com/v5/launches/${launchID}`, function(data) {
+        // JSON result in `data` variable
+        // console.log(data);
+        document.getElementById("patch").src = data.links.patch.large;
+        document.getElementById("name").innerText = data.name;
+        document.getElementById("date").innerText = data.date_utc;
+        document.getElementById("desc").innerText = data.detals;
+        document.getElementById("rocket").innerText = data.rocket;
+        document.getElementById("yt").innerText = data.links.webcast;
+    });
 }
